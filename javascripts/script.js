4718ce72e373d1c82935911384c959f5b19e8e47
cuurjol <cuurjol@gmail.com>
2019-05-19 22:55:17 +0300

    Edited CV.

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,10 @@
 $(function() {
   $('.info-block-header').on('click', function () {
-    if ($(this).find('.fa-angle-up').length) {
-      $(this).find('.fa-angle-up').addClass('fa-angle-down').removeClass('fa-angle-up');
+    if ($(this).find('.fa-minus').length) {
+      $(this).find('.fa-minus').addClass('fa-plus').removeClass('fa-minus');
     }
     else {
-      $(this).find('.fa-angle-down').addClass('fa-angle-up').removeClass('fa-angle-down');
+      $(this).find('.fa-plus').addClass('fa-minus').removeClass('fa-plus');
     }
     $(this).next('.info-block-content').slideToggle(400);
   });
